7987e55375159b452ee5ed215b906b324cd0e469
Bilel-707 <Bilel.bouneb90@gmail.com>
2022-09-22 22:18:44 +0100

    stage test corrected

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  <Provider>
+    render(
+    <App />
+    ); const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  </Provider>;
 });
